f15f137f912831054db51f96b4ef4a114236fb3b
wesley <wesleyaleixo0950@gmail.com>
2023-08-24 14:37:07 -0300

    Preload file from asstes/sample.db

--- lib/cadastro_livro_model.dart
+++ lib/cadastro_livro_model.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CadastroLivroModel {
   late final Database _database;
@@ -10,11 +13,18 @@ class CadastroLivroModel {
       join(await getDatabasesPath(), 'livros_database.db'),
       onCreate: (db, version) {
         return db.execute(
-          'CREATE TABLE livros(id INTEGER PRIMARY KEY, nome TEXT, autor TEXT, classificacao TEXT)',
+          'CREATE TABLE livros(id INTEGER PRIMARY KEY, nome TEXT, autor TEXT, classificacao TEXT, capa TEXT)',
         );
       },
       version: 1,
     );
+
+    bool dataLoaded = await _checkIfDataLoaded();
+    if (!dataLoaded) {
+      await _loadSampleDatabase();
+      await _setDataLoaded();
+    }
+
     await _atualizarListaLivros();
   }
 
@@ -24,7 +34,7 @@ class CadastroLivroModel {
       livro,
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
-    _limparCampos();
+
     await _atualizarListaLivros();
   }
 
@@ -38,9 +48,27 @@ class CadastroLivroModel {
     await _atualizarListaLivros();
   }
 
-  void _limparCampos() {
-    // Implementação para limpar os campos
-  }
 
   List<Map<String, dynamic>> get livros => _livros;
+
+  Future<bool> _checkIfDataLoaded() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('data_loaded') ?? false;
+  }
+
+  Future<void> _setDataLoaded() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('data_loaded', true);
+  }
+
+  Future<void> _loadSampleDatabase() async {
+    String path = join(await getDatabasesPath(), 'livros_database.db');
+    ByteData data = await rootBundle.load('assets/sample.db');
+    List<int> bytes =
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+    await File(path).writeAsBytes(bytes);
+
+    print("Loaded sample database");
+  }
 }
